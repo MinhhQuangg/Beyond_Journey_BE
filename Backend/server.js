6e38b20156d36d9d7432a3616b62040870acd2fd
Quanggg <minhquang030120@gmail.com>
2024-10-17 22:16:35 -0500

    update mongoose and mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,21 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
 
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
