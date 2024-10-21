ece84f74b3b12994d80f3dac9103b8741993025c
Quanggg <minhquang030120@gmail.com>
2024-10-21 04:09:27 -0500

    update backend

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: 'success',
+      data: { users },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
