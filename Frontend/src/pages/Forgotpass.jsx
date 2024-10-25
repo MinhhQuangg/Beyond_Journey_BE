08bf06e585c27015877fa27b259694ad8a93554b
khang phan <kcphan27@gmail.com>
2024-10-25 18:46:38 -0400

    update login and signup, new forgot page

---
+++ Frontend/src/pages/Forgotpass.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Box, Typography, TextField, Button, Link } from "@mui/material";
+import logo from "../assets/logobackground.jpg";
+import { useNavigate } from "react-router-dom";
+
+export const ForgotPassword = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh", // Full viewport height
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center", // Centers the content vertically
+        bgcolor: "#f6f6f4",
+        padding: 3,
+      }}
+    >
+      {/* Logo on the top-left corner */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: "20px",
+          left: "20px",
+        }}
+      >
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ width: "150px", cursor: "pointer" }}
+          onClick={() => navigate("/")}
+        />
+      </Box>
+      {/* Main Content Box */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "left",
+          maxWidth: 400,
+          width: "100%",
+        }}
+      >
+        {/* Header */}
+        <Typography
+          fontWeight="bold"
+          gutterBottom
+          sx={{ mb: "18px", fontSize: "36px" }}
+        >
+          Reset your password
+        </Typography>
+
+        {/* Instructions */}
+        <Box sx={{ mb: "24px" }}>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            textAlign="left"
+            sx={{ mb: "12px" }}
+          >
+            Fear not. We'll email you instructions to reset your password.
+          </Typography>
+        </Box>
+        <Box sx={{ mb: "24px" }}>
+          <Typography
+            sx={{
+              pb: "12px",
+              textAlign: "left",
+              fontSize: "16px",
+              fontWeight: "bold",
+            }}
+          >
+            Email
+          </Typography>
+          {/* Input Field */}
+          <TextField variant="outlined" fullWidth sx={{ mb: "24px" }} />
+        </Box>
+
+        {/* Reset Password Button */}
+        <Box sx={{ display: "flex" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{
+              bgcolor: "#3991cd",
+              ":hover": { bgcolor: "#5d5a7d" },
+              borderRadius: "38px",
+              maxWidth: "250px",
+              lineHeight: "40px",
+              textAlign: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "0 42px",
+              mr: "18px",
+            }}
+          >
+            Reset Password
+          </Button>
+
+          {/* Return to Login Link */}
+          <Link
+            href="#"
+            underline="always"
+            sx={{ mt: 1, color: "#3991cd" }}
+            onClick={() => navigate("/login")}
+          >
+            Return to login
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ForgotPassword;
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -9,14 +9,9 @@ import {
 } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/toursquare.jpg";
-import theme from "../context/theme";
-import pic1 from "../assets/pic/pic4.jpg";
-import pic2 from "../assets/pic/pic5.jpg";
-import pic3 from "../assets/pic/pic6.jpg";
 import axios from "axios";
-
-const images = [pic1, pic2, pic3];
+import theme from "../context/theme";
+import logo from "../assets/logobackground.jpg";
 
 export const SignUp = () => {
   const {
@@ -25,24 +20,10 @@ export const SignUp = () => {
     formState: { errors, isValid },
     watch,
     trigger,
-  } = useForm({
-    mode: "onChange",
-  });
-  const [bgImage, setBgImage] = useState(images[0]);
+  } = useForm({ mode: "onChange" });
+
   const navigate = useNavigate();
-
-  const password = watch("password"); // watch the password for confirm password validation
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setBgImage((prevImage) => {
-        const currentIndex = images.indexOf(prevImage);
-        const nextIndex = (currentIndex + 1) % images.length;
-        return images[nextIndex];
-      });
-    }, 5000);
-    return () => clearInterval(intervalId);
-  }, []);
+  const password = watch("password");
 
   const onSubmit = async (data) => {
     try {
@@ -50,13 +31,10 @@ export const SignUp = () => {
         "http://127.0.0.1:3000/api/v1/users/signup",
         data
       );
-      console.log(response.data.status);
-      if (response.data.status === "success")
-      {navigate("/login")};
+      if (response.data.status === "success") navigate("/login");
     } catch (err) {
       console.log(err);
     }
-    console.log(data);
   };
 
   return (
@@ -64,194 +42,232 @@ export const SignUp = () => {
       <Box
         sx={{
           display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           minHeight: "100vh",
           height: "100vh",
           overflow: "hidden",
+          backgroundColor: "#f5f5f2", // Light background
+          padding: "20px",
         }}
       >
-        {/* Left side with rotating background Image */}
         <Box
           sx={{
-            width: "50%",
-            backgroundImage: `url(${bgImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            transition: "background-image 0.5s ease-in-out",
-          }}
-        />
-
-        {/* Right side with Sign Up Form */}
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#ffffff",
-            height: "100vh",
+            position: "absolute",
+            top: "20px",
+            left: "20px",
           }}
         >
-          <Box
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ width: "150px", cursor: "pointer" }}
+            onClick={() => navigate("/")}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "500px",
+            padding: "40px",
+            borderRadius: "8px",
+            boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "#fff",
+            textAlign: "center",
+          }}
+        >
+          <Typography
             sx={{
-              width: "100%",
-              maxWidth: "400px",
-              backgroundColor: "#fff",
-              padding: "40px",
-              borderRadius: "8px",
-              boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+              mb: 2,
+              textAlign: "left",
+              fontSize: "30px",
+              fontWeight: "bold",
+              mb: "6px",
             }}
           >
-            {/* Logo at the top */}
-            <Box sx={{ textAlign: "center", mb: 1 }}>
-              <img
-                src={logo}
-                alt="Logo"
-                style={{ width: "300px", height: "auto", marginBottom: "10px" }}
-              />
-            </Box>
+            Sign up for BeyondJourney
+          </Typography>
+          <Typography sx={{ mb: "20px", textAlign: "left", fontSize: "13px" }}>
+            Already have an account?{" "}
+            <Link
+              component="button"
+              onClick={() => navigate("/login")}
+              sx={{ color: "#3991cd" }}
+              underline="hover"
+            >
+              Log in
+            </Link>
+          </Typography>
 
-            <Typography variant="h4" sx={{ mb: 2, textAlign: "center" }}>
-              Create your account
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* First Name */}
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              First Name
+            </Typography>
+            <TextField
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("firstName", { required: "First Name is required" })}
+              error={!!errors.firstName}
+              helperText={errors.firstName?.message}
+              onBlur={() => trigger("firstName")}
+            />
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              Last Name
             </Typography>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* First Name */}
-              <TextField
-                label="First Name *"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("name", {
-                  required: "First Name is required",
-                })}
-                error={!!errors.name}
-                helperText={errors.name ? errors.name.message : ""}
-                onBlur={() => trigger("firstName")} // Trigger validation on blur
-              />
-
-              {/* Last Name */}
-              {/* <TextField
-                label="Last Name *"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("lastName", { required: "Last Name is required" })}
-                error={!!errors.lastName}
-                helperText={errors.lastName ? errors.lastName.message : ""}
-                onBlur={() => trigger("lastName")} // Trigger validation on blur
-              /> */}
-
-              {/* Phone Number */}
-              {/* <TextField
-                label="Phone Number *"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("phone", {
-                  required: "Phone Number is required",
-                  pattern: {
-                    value: /^[0-9]+$/, // Ensure only numbers are entered
-                    message: "Phone number must contain only numbers",
-                  },
-                  minLength: {
-                    value: 10,
-                    message: "Phone number must be 10 digits",
-                  },
-                  maxLength: {
-                    value: 10,
-                    message: "Phone number must be 10 digits",
-                  },
-                })}
-                error={!!errors.phone}
-                helperText={errors.phone ? errors.phone.message : ""}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }} // Limit input to numbers
-                onBlur={() => trigger("phone")} // Trigger validation on blur
-              /> */}
-
-              {/* Address */}
-              {/* <TextField
-                label="Address *"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("address", { required: "Address is required" })}
-                error={!!errors.address}
-                helperText={errors.address ? errors.address.message : ""}
-                onBlur={() => trigger("address")} // Trigger validation on blur
-              /> */}
-
-              {/* Email */}
-              <TextField
-                label="Email *"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("email", {
-                  required: "Email is required",
-                  validate: (value) =>
-                    value.includes("@") || 'Email must include "@"',
-                })}
-                error={!!errors.email}
-                helperText={errors.email ? errors.email.message : ""}
-                onBlur={() => trigger("email")} // Trigger validation on blur
-              />
-
-              {/* Password */}
-              <TextField
-                label="Password *"
-                type="password"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("password", { required: "Password is required" })}
-                error={!!errors.password}
-                helperText={errors.password ? errors.password.message : ""}
-                onBlur={() => trigger("password")} // Trigger validation on blur
-              />
-
-              {/* Confirm Password */}
-              <TextField
-                label="Confirm Password *"
-                type="password"
-                variant="outlined"
-                fullWidth
-                sx={{ mb: 2 }}
-                {...register("passwordConfirm", {
-                  required: "Please confirm your password",
-                  validate: (value) =>
-                    value === password || "Passwords do not match",
-                })}
-                error={!!errors.passwordConfirm}
-                helperText={
-                  errors.passwordConfirm ? errors.passwordConfirm.message : ""
-                }
-                onBlur={() => trigger("confirmPassword")} // Trigger validation on blur
-              />
-
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                sx={{ py: 1.5, fontWeight: "bold" }}
-                disabled={!isValid} // Disable button until form is valid
-              >
-                Sign Up
-              </Button>
-            </form>
-
-            <Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>
-              Already have an account?{" "}
-              <Link
-                component="button"
-                onClick={() => navigate("/login")}
-                underline="hover"
-              >
-                Log in here
-              </Link>
+            {/* Last Name */}
+            <TextField
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("lastName", { required: "Last Name is required" })}
+              error={!!errors.lastName}
+              helperText={errors.lastName?.message}
+              onBlur={() => trigger("lastName")}
+            />
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              Phone Number
             </Typography>
-          </Box>
+
+            {/* Phone Number */}
+            <TextField
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("phoneNumber", {
+                required: "Phone Number is required",
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message: "Phone number must contain only numbers",
+                },
+                minLength: {
+                  value: 10,
+                  message: "Phone number must be 10 digits",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "Phone number must be 10 digits",
+                },
+              })}
+              error={!!errors.phoneNumber}
+              helperText={errors.phoneNumber?.message}
+              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              onBlur={() => trigger("phoneNumber")}
+            />
+
+            {/* Email */}
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              Email
+            </Typography>
+            <TextField
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("email", {
+                required: "Email is required",
+                validate: (value) =>
+                  value.includes("@") || 'Email must include "@"',
+              })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              onBlur={() => trigger("email")}
+            />
+
+            {/* Password */}
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              Password
+            </Typography>
+            <TextField
+              type="password"
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("password", { required: "Password is required" })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              onBlur={() => trigger("password")}
+            />
+
+            {/* Confirm Password */}
+            <Typography
+              sx={{
+                pb: "12px",
+                textAlign: "left",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+            >
+              Confirm Password
+            </Typography>
+            <TextField
+              type="password"
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...register("passwordConfirm", {
+                required: "Please confirm your password",
+                validate: (value) =>
+                  value === password || "Passwords do not match",
+              })}
+              error={!!errors.passwordConfirm}
+              helperText={errors.passwordConfirm?.message}
+              onBlur={() => trigger("passwordConfirm")}
+            />
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{
+                py: 1.5,
+                fontWeight: "bold",
+                marginTop: 2,
+                backgroundColor: "#3991cd",
+                ":hover": { backgroundColor: "#5d5a7d" },
+              }}
+              disabled={!isValid}
+            >
+              Sign Up
+            </Button>
+          </form>
         </Box>
       </Box>
     </ThemeProvider>
