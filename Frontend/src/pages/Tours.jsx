8f6242afc8108fe59505e353ccdd21d5353d00cc
quang lieu <minhquang030120@gmail.com>
2024-12-29 03:23:03 -0600

    update tour page

--- Frontend/src/pages/Tours.jsx
+++ Frontend/src/pages/Tours.jsx
@@ -5,10 +5,30 @@ import { blackBackground } from "../assets";
 import { styles } from "../styles";
 import Search from "../components/common/Search";
 import axios from "axios";
-import { Rating } from "@mui/material";
+import { Checkbox, Rating, Slider } from "@mui/material";
+
+const ratings = [5, 4, 3, 2, 1];
+const durations = ["0-3 days", "3-5 days", "over 5 days"];
 
 const Tours = () => {
   const [tours, setTours] = useState([]);
+  const [topTours, setTopTours] = useState([]);
+  const [range, setRange] = useState([100, 900]);
+  const handleChange = (event, newValue) => {
+    setRange(newValue);
+  };
+  useEffect(() => {
+    const fetchTopTour = async () => {
+      const response = await axios.get(
+        "http://127.0.0.1:3000/api/v1/tours/top-5-cheap?limit=3"
+      );
+      const tours = response.data?.data?.tours;
+      const topTour = tours.slice(0, 3);
+      console.log(topTour);
+      setTopTours(topTour);
+    };
+    fetchTopTour();
+  }, []);
   useEffect(() => {
     const fetchTour = async () => {
       const response = await axios.get("http://127.0.0.1:3000/api/v1/tours");
@@ -18,7 +38,7 @@ const Tours = () => {
     fetchTour();
   }, []);
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col">
       <NavBar />
       <div
         className="bg-cover bg-center"
@@ -29,51 +49,51 @@ const Tours = () => {
         }}
       >
         <div className="flex flex-col relative h-full">
-          <div
-            className="flex justify-center items-center flex-col"
-            style={{
-              height: "35vh",
-            }}
-          >
+          <div className="flex justify-center items-center flex-col h-[35vh]">
             <span className="font-sans text-[80px] font-extrabold text-white tracking-wide">
               Tours
             </span>
           </div>
 
-          <div
-            className="absolute top-1/2 left-1/2 z-10 w-[70%]"
-            style={{
-              transform: "translate(-50%, 180%)",
-            }}
-          >
-            <div className="flex justify-center w-full">
+          <div className="flex justify-center items-center w-full h-[0vh] mt-auto">
+            <div className="w-[65%]">
               <Search />
             </div>
           </div>
         </div>
       </div>
-      <div className="mt-4 bg-white-100">
+      <div className="mt-10 bg-white-100">
         <div className={`${styles.paddingTour} grid grid-cols-8 gap-10`}>
           <div className="col-span-2 flex flex-col gap-10">
             <div className="rounded-2xl flex flex-col gap-3 shadow-lg p-7">
               <div className="font-bold text-[20px]">Filter by Price</div>
-              <div>Filter by Price</div>
-              <div>Price:</div>
+              <Slider
+                value={range}
+                onChange={handleChange}
+                valueLabelFormat={(value) => `${value}`}
+                min={100}
+                max={900}
+              />
+              <div>
+                <span>Price: &nbsp;</span>
+                <span className="font-bold">{`$${range[0]}`} - </span>
+                <span className="font-bold">{`$${range[1]}`}</span>
+              </div>
             </div>
-            <div className="rounded-2xl flex flex-col gap-3 shadow-lg p-7">
+            <div className="rounded-2xl flex flex-col shadow-lg p-7">
               <div className="font-bold text-[20px]">Filter by Review</div>
-              <div>Filter by Review</div>
-              <div>Price:</div>
+              {ratings.map((rating) => (
+                <div key={rating} className="flex items-center">
+                  <Checkbox />
+                  <Rating defaultValue={rating} readOnly />
+                </div>
+              ))}
             </div>
             <div className="rounded-2xl flex flex-col gap-3 shadow-lg p-7">
               <div className="font-bold text-[20px]">Filter by Duration</div>
-              <div>Filter by Duration</div>
-              <div>Price:</div>
             </div>
             <div className="rounded-2xl flex flex-col gap-3 shadow-lg p-7">
               <div className="font-bold text-[20px]">Top Rated Tour</div>
-              <div>Filter by Price</div>
-              <div>Price:</div>
             </div>
           </div>
           <div className="col-span-6">
@@ -91,7 +111,7 @@ const Tours = () => {
                     <img
                       src={`http://127.0.0.1:3000${el.imageCover}`}
                       alt={i}
-                      className=" h-[280px] w-full hover:brightness-90 cursor-pointer"
+                      className=" h-[250px] w-full hover:brightness-90 cursor-pointer"
                     />
                     <div
                       className="absolute bg-white rounded-2xl z-10 w-full p-[20px]"
@@ -115,16 +135,16 @@ const Tours = () => {
                       <div className="text-[18px]">
                         {el.startLocation.description}
                       </div>
-                      <div className="text-[30px] font-bold hover:text-primary_2 cursor-pointer">
+                      <div className="text-[25px] font-bold hover:text-primary_2 cursor-pointer">
                         {el.name}
                       </div>
                       <div className="my-2">
                         From:
-                        <span className="text-[18px] text-primary_2 font-bold ">
+                        <span className="text-[18px] text-primary_2 font-bold ml-1">
                           ${el.price}
                         </span>
                       </div>
-                      <div className="flex justify-center items-center mt-5 invisible group-hover:visible transition-opacity duration-300">
+                      <div className="flex justify-center items-center invisible group-hover:visible transition-opacity duration-300">
                         <button className="w-[80%] bg-primary_4 hover:bg-primary_2 hover:text-white p-2 text-[18px] rounded-2xl">
                           More Information
                         </button>
